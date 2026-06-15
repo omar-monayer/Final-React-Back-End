4db0e9a25d72ca729dd0dfd8bc5f04087a85d799
omar <103359026+omar-monayer@users.noreply.github.com>
2026-06-16 02:31:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ pgclient
   .then(() => {
     console.log("Connected to PostgreSQL database");
 
-    app.listen(PORT, () => {
+    app.listen(PORT,  "0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
